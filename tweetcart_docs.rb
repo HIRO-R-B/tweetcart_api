3cd798f1638a29f5b7924781a169225f54cdd58b
HIRO-R-B <35461694+HIRO-R-B@users.noreply.github.com>
2021-05-08 07:42:58 -0700

    Updated tweetcart_docs.rb

--- tweetcart_docs.rb
+++ tweetcart_docs.rb
@@ -30,12 +30,13 @@ S
       :docs_math,
       :docs_summary,
       :docs_args,
+      :docs_outputs,
       :docs_inputs,
       :docs_keyboard,
       :docs_mouse,
-      :docs_outputs,
       :docs_geometry,
       :docs_primitive_conversions,
+      :docs_ffidraw,
       :docs_enumerable,
       :docs_array,
       :docs_hash,
@@ -124,18 +125,73 @@ S
 
 **** CONSTANTS
 #+begin_src
-  F  | 255
-  W  | args.grid.w
-  H  | args.grid.h
-  Z  | [0]
-  PI | Math::PI
-  E  | Math::E
+  F   | 255
+  G   | 127
+  W   | args.grid.w
+  H   | args.grid.h
+  N   | [nil]
+  Z   | [0]
+  S30 | 30.sin
+  S60 | 60.sin
+  PI  | Math::PI
+  E   | Math::E
 #+end_src
 
-**** ~#CI~
+**** Methods
+**** ~#CI x, y, radius, r = 0, g = 0, b = 0, a = 255~
+Returns a circle sprite (as array) with a given ~radius~ that is centered at ~x~ and ~y~
+
+**** ~#TR x, y, side_length, angle=0, r=0, g=0, b=0, a=255~
+Returns an equilateral triangle sprite (as hash) with a given ~side_length~ centered at ~x~ and ~y~
+
+**** ~#PLY points, r = nil, g = nil, b = nil, a = nil~
+Returns an array of lines that form an unfilled polygon
+
+**** ~#PLYP points, r = nil, g = nil, b = nil, a = nil~
+Returns an array of lines that form a path
+
+**** Modules
+**** ~P~
+P provides methods to facilitate the creation of primitive classes with draw overrides
+
+**** ~#do *attrs, &draw_override~
+Returns a class with the given attrs defined and block as its draw_override
+The returned class can be initialized with key arguments
 #+begin_src
-  def CI(x, y, radius, r = 0, g = 0, b = 0, a = 255)
-    [radius.to_square(x, y), :c, 0, a, r, g, b].sprite
+  def t a
+    Thing ||= P.do(:shift, :x, :y) { |ffi| ffi.dso(@x, @y, @shift, 100, 0, 0, 0, 255) }
+    t = Thing.new(x: a.m.x, y: a.m.y)
+    t.shift = (100 * a.t.sin).a
+    a.o.pr << t # Displays a black square with a shifting width at your mouse location
+  end
+#+end_src
+
+**** ~#(so|sp|la|li|bo) *attrs, &draw_override~
+Like ~#do~ except they return a class with the relevant attrs for their primitives predefined
+#+begin_src
+  def t a
+    Sprite ||= P.sp { |ffi| ffi.dsp(@x, @y, @w, @h, :p, @an, 255, 0, 0, 0) }
+    s = Sprite.new(w: 100, h: 100)
+    s.x, s.y = a.m.p
+    a.o.sp << s # Displays a black square at your mouse location
+  end
+#+end_src
+
+**** ~#d(so|sp|la|li|bo) *attrs, &block~
+Returns more specialized primitive classes where you can give ordered and keyword arguments and the given block is called before draw_override  
+
+Note: ~#dsp(path=nil, *attrs, &block)~ Dsp has an initial path argument. The path argument can be used as a default for any instanced sprites
+#+begin_src
+  # Creates moving white circles wherever you click
+  def t a
+    S ||= P.dsp(:c,:ix,:iy) do
+      @dx = 25*a.t.sin
+      @dy = 25*a.t.cos
+      @x  = @ix + @dx
+      @y  = @iy + @dy
+    end
+    a.bg=[0,0,0]
+    _SP! S.new(w: 100, h: 100, ix: a.m.x, iy: a.m.y) if a.m.c
   end
 #+end_src
 #{TweetcartDocs.format_aliases GTK::MainTweetcart.aliases}
@@ -146,7 +202,18 @@ S
   def docs_args
     <<-S
 *** args
+**** Methods
+**** ~#vp x, y, w, h, r = 0, g = 0, b = 0~
+Push solids into outputs.primitives that cover everything but the specified area
 #{TweetcartDocs.format_aliases GTK::Args::Tweetcart.aliases}
+
+S
+  end
+
+  def docs_outputs
+    <<-S
+*** args.outputs
+#{TweetcartDocs.format_aliases GTK::Outputs::Tweetcart.aliases}
 
 S
   end
@@ -175,14 +242,6 @@ S
 S
   end
 
-  def docs_outputs
-    <<-S
-*** args.outputs
-#{TweetcartDocs.format_aliases GTK::Outputs::Tweetcart.aliases}
-
-S
-  end
-
   def docs_geometry
     <<-S
 *** args.geometry
@@ -198,6 +257,24 @@ S
 Available on Arrays and Hashes
 #{TweetcartDocs.format_aliases GTK::Primitive::ConversionCapabilities::Tweetcart.aliases}
 
+S
+  end
+
+  def docs_ffidraw
+    <<-S
+*** FFIDraw
+**** Methods
+**** ~#d(so|sp|la|li|bo)~
+Short aliases for ffi_draw methods that are used in draw overrides
+#+begin_src
+  class SomeSolid
+    # solid things
+
+    def draw_override(ffi_draw)
+      ffi_draw.dso(@x, @y, @w, @h) # Unspecified parameters will be set to nil
+    end
+  end
+#+end_src
 S
   end
 
@@ -228,32 +305,14 @@ S
   def docs_numeric
     <<-S
 *** Numeric
-**** ~#r~
+**** Methods
 #+begin_src
-  def r
-    rand_ratio.to_i
-  end
-#+end_src
-
-**** ~#dm~
-#+begin_src
-  def dm x
-    divmod x
-  end
-#+end_src
-
-**** ~#sin~
-#+begin_src
-  def sin
-    Math.sin(self.to_radians)
-  end
-#+end_src
-
-**** ~#cos~
-#+begin_src
-  def cos
-    Math.cos(self.to_radians)
-  end
+  r   = rand_ratio.to_i
+  fl  = floor
+  ce  = ceil
+  dm  = divmod
+  sin
+  cos
 #+end_src
 #{TweetcartDocs.format_aliases GTK::NumericTweetcart.aliases}
 
@@ -280,13 +339,31 @@ S
   def docs_symbol
     <<-S
 *** Symbol
-**** ~#[]~
+**** Methods
+**** ~#(so|sp|pr|la|li|bo|de)~
+Shorthand methods to access and push into render targets
+#+begin_src
+  def t a
+    :apple.so << [0, 0, 50, 50, 0, 0, 0] # Create rt :apple and push a black square into solids
+    a.o.sp << [0, 0, 1280, 720, :apple, a.t] # Display a rotating square using :apple
+  end
+#+end_src
+
+**** ~#[] *args, &block~
+Returns a lambda that will send the given method name and arguments to an object
 #+begin_src
   def [] *args, &block
     -> caller, *rest { caller.send self, *rest, *args, &block }
   end
+
+  # This allows for syntax like:
+  #-> [1, 2, 3].map &:add[5]
+  #=> [6, 7, 8]
+
+  #-> fn = :map[] { |i| i.map { i } }
+  #-> fn.call([1, 2, 3])
+  #=> [[1],[2, 2],[3, 3, 3]]
 #+end_src
-This allows for syntax like ~[1, 2, 3].map &:add[5]~
 
 S
   end
